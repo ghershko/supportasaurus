2cc02d61ad533cf5b55e3f860d63189f52ed166d
Guy Hershko <guy.h@bookaway.com>
2024-01-28 17:50:52 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express';
+import { config } from 'dotenv';
+import OpenAI from 'openai';
+import bolt from '@slack/bolt';
+
+config();
+
+// const app = express();
+// app.use(express.json());
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = new bolt.App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+});
+  
+
+// app.post('/ask-gpt', async (req: Request, res: Response) => {
+//     try {
+//         const { prompt } = req.body;
+//         const gptResponse = await openai.chat.completions.create({
+//             messages: [{ role: 'user', content: prompt }],
+//             model: 'gpt-4-1106-preview',
+//         });
+
+//         res.json(gptResponse);
+//     } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+// const PORT = process.env.PORT || 9000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+app.message('hello', async ({ message, say }) => {
+    await say(`Hey there!`);
+  });
+
+(async () => {
+    // Start the app
+    await app.start(process.env.PORT || 9000);
+  
+    console.log('⚡️ Bolt app is running!');
+  })();
