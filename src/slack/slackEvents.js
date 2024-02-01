5d38d14fdf49736c8085ceab779fb551bba112b1
Guy Hershko <guy.h@bookaway.com>
2024-02-01 13:58:32 +0200

    es 6 fix

--- src/slack/slackEvents.js
+++ src/slack/slackEvents.js
@@ -1,31 +1,32 @@
 const { countWeeksUntilNextOnCall } = require("../logic/onCallLogic");
 
- const setupSlackEvents = (app) => {
-    // app.event('app_mention', async ({ event, say }) => {
-    //     await say(`Hello <@${event.user}>!`);
-    // });
+  const setupSlackEvents = (app) => {
+      // app.event('app_mention', async ({ event, say }) => {
+      //     await say(`Hello <@${event.user}>!`);
+      // });
 
     app.command('/when-is-my-turn', async ({ command, ack, say, client }) => {
-        await ack();
-        const args = command.text.split(' ');
-        const [firstName] = args;
+      await ack();
+      const args = command.text.split(' ');
+      const [firstName] = args;
 
-        const userInfo = await client.users.info({ user: command.user_id });
-        const userName = (userInfo.user?.real_name || userInfo.user?.name)?.split(' ')[0];
+      const userInfo = await client.users.info({ user: command.user_id });
+      const userName = (userInfo.user?.real_name || userInfo.user?.name)?.split(' ')[0];
 
-        const name = firstName || userName
+      const name = firstName || userName
 
-        if(name) {
-            const nextOnCall = countWeeksUntilNextOnCall(name);
-            await say(
-                weeksUntil 
-                    ? `📆 Just a friendly countdown: ${nextOnCall} weeks until your on-call adventure begins!` 
-                    : "🌟 It's your turn to shine! You're on call this week. Good luck!"
-            );
+      if(name) {
+        const nextOnCall = countWeeksUntilNextOnCall(name);
+        if(weeksUntil) {
+            await say(`📆 Just a friendly countdown: ${nextOnCall} weeks until your on-call adventure begins!`);
         }
         else {
-            await say(`I can't find your name, please enter it as a parameter as such: /when-is-my-turn <NAME>`);
+            await say("🌟 It's your turn to shine! You're on call this week. Good luck!")
         }
+      }
+      else {
+        await say(`I can't find your name, please enter it as a parameter as such: /when-is-my-turn <NAME>`);
+      }
     });
 }
 
