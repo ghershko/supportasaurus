ecee3b62e3becab97c8440d3fdb41ff81b471547
Guy Hershko <guy.h@bookaway.com>
2024-03-23 22:17:59 +0200

    logging

--- src/services/decodeGoogleCredentials.service.js
+++ src/services/decodeGoogleCredentials.service.js
@@ -5,6 +5,7 @@ const { credentialsBase64 } = require('../config');
 // for production
 const decodeGoogleCredentials = () => {
     if (credentialsBase64) {
+      console.log('writing GOOGLE_APPLICATION_CREDENTIALS...')
       const credentialsBuffer = Buffer.from(credentialsBase64, 'base64');
       const credentialsJSON = credentialsBuffer.toString('utf-8');
       const filePath = path.join(__dirname, 'google-credentials.json');
